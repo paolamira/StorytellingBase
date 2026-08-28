294c677dafe0d12488561f5dfc1ddbd1cae774d3
paolamira <paola.mira@escola.pr.gov.br>
2026-08-28 11:14:32 -0300

    paola

--- script.js
+++ script.js
@@ -6,73 +6,71 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "pergunta deve ser escrita aqui",
+        enunciado: "Você nota que está no terceirão e, com isso, vêm o vestibular e o Enem. Como decide agir?",
         alternativas: [
             {
-                texto: "alternativa letra A deve ser escrita aqui",
-                afirmacao: "Afirmação gerada se a pessoa escolher a letra A deve estar aqui "
+                texto: "Reconhece a dificuldade e começa a estudar para as provas desde o começo.",
+                afirmacao: "Reconhecer a dificuldade e começar a estudar desde o início ajudou você a se preparar com mais segurança para o vestibular e o Enem."
             },
             {
-                texto: "alternativa letra B deve ser escrita aqui",
-                afirmacao: "Afirmação gerada se a pessoa escolher a letra B deve estar aqui "
+                texto: "Decide agir com negligência por não saber o que quer cursar e, além disso, pelo desânimo.",
+                afirmacao: "A indecisão sobre o curso e o desânimo fizeram com que você negligenciasse os estudos e deixasse a preparação para depoiis."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Quando você pisca, nota que já está na metade do ano; portanto, a pressão para o vestibular e o Enem aumenta.",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Sente-se segura em relação a isso por ter lidado com esse problema desde o começo do ano.",
+                afirmacao: "Ter começado a se preparar desde o início do ano fez com que você se sentisse mais segura diante da pressão social em relação do vestibular e do Enem."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Começa a se sentir culpada por ter negligenciado o seu futuro. Além disso, vê seus amigos extremamente confiantes em relação aos estudos.",
+                afirmacao: "A culpa por ter negligenciado os estudos, somada à comparação com seus amigos, aumentou a pressão e a insegurança em relação ao seu futuro."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Já estamos em outubro, faltando um mês para as provas começarem. Como você está se sentindo?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Permanece segura, está finalizando alguns conteúdos e se sente pronta para encarar as provas.",
+                afirmacao: "Manter uma rotina de estudos desde o início permitiu que você chegasse a outubro mais segura e preparada para as provas."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Começa a se sentir cada vez mais desconfortável e insegura. Tenta encaixar os estudos na sua rotina.",
+                afirmacao: "A falta de preparação ao longo do ano fez com que você chegasse perto das provas insegura e precisasse adaptar sua rotina para conseguir estudar."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "No fim, já é dezembro. Você já terminou todas as provas. Como você lidou com isso?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Você lidou da melhor forma, sabia a maioria dos conteúdos e sente que irá entrar em uma faculdade.",
+                afirmacao: "A preparação ao longo do ano fez com que você chegasse às provas mais confiante, dominando a maior parte dos conteúdos e acreditando na possibilidade de ingressar em uma faculdade."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Você tenta estudar, mas, no dia da prova, esquece o pouco que sabia por conta da ansiedade.",
+                afirmacao: "A falta de preparação e a ansiedade durante as provas dificultaram a concentração e fizeram com que você esquecesse conteúdos que havia estudado."
             }
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Os resultados chegaram no site. Como você se saiu?",
         alternativas: [
             {
-                texto: "",
-                afirmacao: ""
+                texto: "A ansiedade atacou ao entrar no site, mas todo o seu empenho valeu a pena: passou em quinto lugar na faculdade de Direito.",
+                afirmacao: "Mesmo enfrentando a ansiedade no momento do resultado, todo o esforço e a preparação ao longo do ano contribuíram para que você conquistasse o quinto lugar em Direito."
             },
             {
-                texto: "",
-                afirmacao: ""
+                texto: "Você entra no site por pressão da família. Você rola e rola e não vê o seu nome. Então, chega ao final da lista: você não passou e nem na lista de espera está.",
+                afirmacao: "A falta de preparação ao longo do ano resultou na reprovação no vestibular, fazendo com que você percebesse as consequências de ter negligenciado os estudos."
             }
         ]
-    },
+    }
 ];
-
-
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
